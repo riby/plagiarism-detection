9982718dd28cccc4dcf7bba8e0cb3233dfc713d3
riby <ravneet.sdh@gmail.com>
2015-11-20 20:10:59 -0600

    File Upload code

---
+++ src/main/java/uta/cse/algo/Controller.java
@@ -0,0 +1,25 @@
+package uta.cse.algo;
+
+/**
+ * Created by riby on 11/20/15.
+ */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+
+public class Controller {
+/*
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+*/
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Controller.class, args);
+    }
+}
---
+++ src/main/java/uta/cse/algo/FileModel.java
@@ -0,0 +1,11 @@
+package uta.cse.algo;
+
+/**
+ * Created by riby on 11/20/15.
+ */
+public class FileModel {
+
+    //@Id
+    private String id;
+    private String name;
+}
